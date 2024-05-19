e1a0b1f93266ee06ad2d638773fb0452a1a9bef6
adamelhorri <adamelhorri2@gmail.com>
2024-05-19 16:34:30 +0200

    ajout des srappings categorie wiki et JDM

---
+++ jdm_scrapping.py
@@ -0,0 +1,22 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+def check_relation(mot1, relation, mot2):
+    url = f"https://www.jeuxdemots.org/rezo-ask.php?gotermsubmit=Demander&term1={mot1}&rel={relation}&term2={mot2}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # Check if the content contains "oui"
+        if "oui" in soup.get_text():
+            return True
+    return False
+
+# Example usage:
+mot1 = "chien"
+relation = "r_isa"
+mot2 = "animal"
+
+result = check_relation(mot1, relation, mot2)
+print(result)
---
+++ wiki_crapping.py
@@ -0,0 +1,53 @@
+from nltk.corpus import stopwords
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from typing import List
+import requests
+from bs4 import BeautifulSoup
+def get_wikipedia_summary(link: str, val: int) -> str:
+    source = urlopen(link).read()
+    soup = BeautifulSoup(source, 'lxml')
+
+    cpt = 0
+    paras = []
+    for paragraph in soup.find_all('p'):
+        if cpt >= val:
+            break
+        parent_div = paragraph.parent
+        if parent_div.name == 'div' and 'mw-content-ltr' in parent_div.get('class', []):
+            paras.append(paragraph.text)
+            cpt += 1
+    return " ".join(paras)
+
+
+def get_wikipedia_article_links(category_url: str) -> List[str]:
+    links = []
+    response = requests.get(category_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for item in soup.find_all('div', {'class': 'mw-category-group'}):
+        for link in item.find_all('a'):
+            href = link.get('href')
+            # Vérification pour s'assurer que le lien est complet
+            if str(href).startswith('/wiki/'):
+                links.append("https://fr.wikipedia.org" + href)
+    return links
+
+
+def scrape_wikipedia_category(category_url: str, val: int) -> List[str]:
+    article_links = get_wikipedia_article_links(category_url)
+    summaries = []
+    for link in article_links:
+        summary = get_wikipedia_summary(link, val)
+        summaries.append(summary)
+    return summaries
+texte=""
+# Exemple d'utilisation :
+category_url = "https://fr.wikipedia.org/wiki/Cat%C3%A9gorie:Chat"
+summaries = scrape_wikipedia_category(category_url, 2)
+
+for summary in summaries:
+    print("*************************************************")
+    print(summary)
+    print("*************************************************")
+   
+txt=texte
